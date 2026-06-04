bc209c354e82dee43f81b7540ebdc1a9b8e437d1
agusnucotti <agus.nucotti@gmail.com>
2026-06-04 18:23:53 -0300

    fix: corregir __file__ en test_integration

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.insert(0, os.path.join(os.path.dirname(_file_), '..', '..'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..'))
 
 from app.app import app
 
